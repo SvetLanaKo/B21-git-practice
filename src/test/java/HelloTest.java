8eeec3544da70ccc23a9730b54918aae392e115b
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-02-07 01:35:21 -0600

    added a new branch

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,6 +1,6 @@
 public class HelloTest {
     public static void main(String[] args) {
-        System.out.println("Hello Test");
+        System.out.println("Hello Group 22");
         //a new line is added for the comment
     }
 }
