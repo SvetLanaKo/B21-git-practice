c7aa055ddb715f9a2b49d89f776ffe5b3d86d477
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-02-07 01:15:48 -0600

    added a new comment in Feature-a Class

--- src/main/java/Feature_a.java
+++ src/main/java/Feature_a.java
@@ -1,6 +1,7 @@
 public class Feature_a {
     public static void main(String[] args) {
         System.out.println("New practice to create a remote repo");
+        //a new line is added
     }
 
 }
