f9ed9a34e85b5c56973a10bc567342d15a794a5c
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-02-07 01:45:35 -0600

    dropped a commit to go back to ConflictBranch Class

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,6 +3,6 @@ public class HelloTest {
         System.out.println("Hello Group 22");
         //a new line is added for the comment
         System.out.println("I need to create a project from scratch");
-
+        //a commit was dropped
     }
 }
