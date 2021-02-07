dfd99b0d88b52aba56813966f36dac9b9d923c41
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-02-07 01:43:21 -0600

    added a new line to Hello Class

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,6 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello Test");
         //a new line is added for the comment
+        System.out.println("a new line was created");
     }
 }
