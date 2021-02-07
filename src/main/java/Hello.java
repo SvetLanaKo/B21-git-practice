c0899b0b194b47dc4c98192af14e89220ebb808b
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-02-07 00:58:36 -0600

    added a new statement in Hello Class

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Practice to create a project");
+        System.out.println("Practice GitHub");
     }
 }
--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,6 +2,5 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello Test");
         //a new line is added for the comment
-
     }
 }
