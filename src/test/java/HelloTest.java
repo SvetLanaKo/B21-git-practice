8c64f5ba2667207bf7422c91ad46e07ecb47f87e
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-02-07 01:41:23 -0600

    added a new statement to Hello Class

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello Group 22");
         //a new line is added for the comment
+        System.out.println("I need to create a project from scratch");
+
     }
 }
