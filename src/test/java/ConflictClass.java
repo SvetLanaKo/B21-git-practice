53ff51a0832136bf29bfc56d996cc2658306769e
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-02-08 14:40:14 -0600

    added another comment line

--- src/test/java/ConflictClass.java
+++ src/test/java/ConflictClass.java
@@ -2,4 +2,5 @@ public class ConflictClass {
     //practicing a conflict situations
     //practicing how to check the difference between Master and Conflict branches
     //something went wrong I dont see the work which i just completed
+    //I have to practice more on git and github
 }
